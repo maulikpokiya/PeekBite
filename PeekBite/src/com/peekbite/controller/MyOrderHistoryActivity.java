be3f0b58b5207ca1e86ae8732ba9c6a855f006e3
maulikpokiya <maulik.pokiya@gmail.com>
2014-05-12 21:34:16 -0400

    Added getting order history from database.

--- PeekBite/src/com/peekbite/controller/MyOrderHistoryActivity.java
+++ PeekBite/src/com/peekbite/controller/MyOrderHistoryActivity.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,12 +23,28 @@ import android.widget.Toast;
 
 import com.application.peekbite.R;
 import com.peekbite.model.OrderHistoryItem;
+import com.peekbite.prototype.HomeScreenActivity;
+import com.peekbite.prototype.OrderScreen;
+import com.peekbite.registration.JSONParser;
 
 public class MyOrderHistoryActivity extends Activity{
 	
 	private List<OrderHistoryItem> orderItemArray;
 	
 	private final static String TAG = "MyOrderHistoryActivity";
+	private String Server = "http://peekbite.pinaksaha.me/";
+	// url to save order items
+	private String url_get_orders = Server + "get_history.php";
+	private List<Integer> cartIds = new ArrayList<Integer>();
+	private List<Integer> restIds = new ArrayList<Integer>();
+	private List<String> restNames = new ArrayList<String>();
+	private List<String> cartDates = new ArrayList<String>();
+	private List<String> fNames = new ArrayList<String>();
+	private List<String> fPrices = new ArrayList<String>();
+
+	JSONParser jsonParser = new JSONParser();
+	// JSON Node names
+	private static final String TAG_SUCCESS = "success";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +119,85 @@ public class MyOrderHistoryActivity extends Activity{
 		Log.i(TAG, "in onItemClick");//test
 		OrderHistoryItem item = orderItemArray.get(position);
 		Toast.makeText(this, "test onItemclick in MyOrderHistoryActivity at position: "+item.getOrderId(), Toast.LENGTH_SHORT).show();
+	}
+	
+	/**
+	 * Background Async Task to get Order history from database
+	 * 
+	 * @author Maulik
+	 * */
+	class getOrders extends AsyncTask<String, String, String> {
+
+		/**
+		 * Storing user
+		 * */
+		protected String doInBackground(String... args) {
+			try {
+				JSONArray cart_Ids = null;
+				JSONArray rest_Ids = null;
+				JSONArray rest_Names = null;
+				JSONArray cart_Dates = null;
+				JSONArray f_Names = null;
+				JSONArray f_Prices = null;
+				
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("uname", args[0].toString()));
+
+				// getting JSON Object
+				JSONObject json = jsonParser.makeHttpRequest(url_get_orders,
+						"POST", params);
+
+				// check log cat for response
+				Log.d("JSON",
+						"JSON Response in MainActivity :" + json.toString());
+
+				// check for success tag
+				int success = json.getInt(TAG_SUCCESS);
+
+				if (success == 1) {
+
+					cart_Ids = json.getJSONArray("cart_id");
+					rest_Ids = json.getJSONArray("rest_id");
+					rest_Names = json.getJSONArray("rest_name");
+					cart_Dates = json.getJSONArray("cart_date");
+					f_Names = json.getJSONArray("fname");
+					f_Prices = json.getJSONArray("fprice");
+
+					if (cart_Ids != null && rest_Ids != null
+							&& cart_Dates != null && cart_Ids != null && f_Names != null
+							&& f_Prices != null) {
+						for (int i = 0; i < cart_Ids.length(); i++) {
+							cartIds.add(cart_Ids.getInt(i));
+							restIds.add(rest_Ids.getInt(i));
+							restNames.add(rest_Names.get(i).toString());
+							cartDates.add(cart_Dates.get(i).toString());
+							fNames.add(f_Names.get(i).toString());
+							fPrices.add(f_Prices.get(i).toString());
+						}
+					}
+
+				} else {
+					// failed to create User
+					MyOrderHistoryActivity.this.runOnUiThread(new Runnable() {
+						public void run() {
+							// your alert dialog builder here
+							new AlertDialog.Builder(MyOrderHistoryActivity.this)
+									.setIcon(android.R.drawable.ic_dialog_alert)
+									.setTitle("Error")
+									.setMessage(
+											"Oops, Error occurred while getting order history.")
+									.setPositiveButton("OK", null).show();
+						}
+					});
+				}
+			} catch (JSONException je) {
+				Log.e("ERROR",
+						"Error in MyOrderHistoryActivity.getorders()" + je.toString());
+			}
+			return "Success";
+		}
+
 	}
 	
 	
